3d6412eb2c416ff66964ee30344ea39d75f64ec1
Jfelipemendez21 <jfelipemendez2103@gmail.com>
2026-08-02 21:09:07 -0500

    .

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -23,5 +23,5 @@ export const env = {
     secret: process.env.JWT_SECRET || 'super_secret_jwt_key_senior_fullstack_2026',
     expiresIn: process.env.JWT_EXPIRES_IN || '8h',
   },
-  corsOrigin: process.env.CORS_ORIGIN || 'http://localhost:5173',
+  corsOrigin: '*',
 };
